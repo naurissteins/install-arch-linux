4757c9c57233a27294752be5260525d65c928a3e
Nauris Steins <nauris.steins@gmail.com>
2021-03-14 18:57:34 +0200

    Battery Saving (laptops only) #1

--- laptop.sh
+++ laptop.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-# Battery saveing
+# Use this script only if you running Arch Linux on laptop
+# Battery saving
 pacman -S --noconfirm acpi acpi_call tlp
 systemctl enable acpid
 systemctl enable tlp

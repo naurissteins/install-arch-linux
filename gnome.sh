9ba88c7a6cc57d4779ef9c8b6ba1704e324b4ac7
Nauris Steins <nauris.steins@gmail.com>
2021-03-14 19:53:39 +0200

    Gnome Desktop Environment

--- gnome.sh
+++ gnome.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 # Gnome Desktop Environment
-git clone https://aur.archlinux.org/yay.git
-cd yay/
-makepkg -si --noconfirm
 
-sudo yay -S --noconfirm xorg gdm gnome gnome-extra gnome-tweaks chrome-gnome-shell arc-gtk-theme arc-icon-theme
+sudo pacman -S --noconfirm xorg gdm gnome gnome-extra gnome-tweaks chrome-gnome-shell arc-gtk-theme arc-icon-theme
 
 sudo systemctl enable gdm
 /bin/echo -e "\e[1;32mREBOOTING IN 5..4..3..2..1..\e[0m"

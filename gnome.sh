f35e6dcfbf87f48b31dd9e2a55e84186dcd7b73e
Nauris Steins <nauris.steins@gmail.com>
2021-03-14 19:01:55 +0200

    Gnome Desktop Environment #1

--- gnome.sh
+++ gnome.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+# Gnome Desktop Environment
 git clone https://aur.archlinux.org/yay.git
 cd yay/
 makepkg -si --noconfirm
@@ -9,3 +9,4 @@ sudo yay -S --noconfirm xorg gdm gnome gnome-extra gnome-tweaks chrome-gnome-she
 sudo systemctl enable gdm
 /bin/echo -e "\e[1;32mREBOOTING IN 5..4..3..2..1..\e[0m"
 sleep 5
+reboot

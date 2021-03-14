6b7b3414cdade476706f282af6467fd9fa452b53
Nauris Steins <nauris.steins@gmail.com>
2021-03-14 19:55:21 +0200

    KDE Plasma Desktop Environment

--- kde.sh
+++ kde.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 # KDE Plasma Desktop Environment
-git clone https://aur.archlinux.org/yay.git
-cd yay/
-makepkg -si --noconfirm
-
-#pikaur -S --noconfirm system76-power
-#sudo systemctl enable --now system76-power
-#sudo system76-power graphics integrated
-#pikaur -S --noconfirm auto-cpufreq
-#sudo systemctl enable --now auto-cpufreq
 
 sudo pacman -S --noconfirm xorg sddm plasma kde-applications materia-kde
 

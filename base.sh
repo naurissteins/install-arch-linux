7e245013eaa7d1020c1f30c4e2f86a1bb26e0248
Nauris Steins <nauris.steins@gmail.com>
2021-03-14 15:30:27 +0200

    First

---
+++ base.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Set Local time (ls /usr/share/zoneinfo)
+timedatectl set-timezone Europe/Riga
+timedatectl set-ntp true
+
+# Uncomment US and LV UTF-8 language
+sed -i '177s/.//' /etc/locale.gen
+sed -i '332s/.//' /etc/locale.gen
+locale-gen
+
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+echo "archlinux" >> /etc/hostname
+echo "127.0.0.1 localhost" >> /etc/hosts
+echo "::1       localhost" >> /etc/hosts
+echo "127.0.1.1 archlinux.localdomain archlinux" >> /etc/hosts
+
+# Change root password to archlinux (please change it later)
+echo root:archlinux | chpasswd
+
+# Install packages. Default AMD CPU
+pacman -S --noconfirm grub efibootmgr efivar networkmanager linux-headers base base-devel bash-completion network-manager-applet dialog wpa_supplicant mtools dosfstools reflector avahi xdg-user-dirs xdg-utils gvfs gvfs-smb nfs-utils inetutils dnsutils bluez bluez-utils cups hplip alsa-utils pulseaudio bash-completion openssh rsync reflector acpi acpi_call tlp virt-manager qemu qemu-arch-extra ovmf bridge-utils dnsmasq vde2 openbsd-netcat ebtables iptables ipset firewalld flatpak sof-firmware nss-mdns acpid
+
+###### CPU #####
+# AMD
+# pacman -S amd-ucode
+
+# Intel
+# pacman -S intel ucode
+
+##### GPU #####
+# AMD
+# pacman -S --noconfirm xf86-video-amdgpu mesa vulkan-radeon
+
+# Nvidia
+# pacman -S --noconfirm nvidia nvidia-utils nvidia-settings mesa
